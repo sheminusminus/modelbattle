0df94d6fc3fe709321a1f50afe01f435bba51bf1
Emily Kolar <emkolar@gmail.com>
2020-03-10 06:25:49 -0700

    add dist build

---
+++ build/precache-manifest.af9cdfd7626a94e7e39a39e2dcc312a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625ee4b3d144689fe690522cee46f634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb443360fc124098ccf7",
+    "url": "/static/css/main.6bf3b75d.chunk.css"
+  },
+  {
+    "revision": "08f120f894d436d88b53",
+    "url": "/static/js/2.d45410d5.chunk.js"
+  },
+  {
+    "revision": "4c11959c0578fa7ec3d93671cefa63ed",
+    "url": "/static/js/2.d45410d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb443360fc124098ccf7",
+    "url": "/static/js/main.886bb098.chunk.js"
+  },
+  {
+    "revision": "3e96553d3f85d923008d",
+    "url": "/static/js/runtime-main.b2461fc2.js"
+  }
+]);
