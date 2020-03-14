7e164db80cab6f2ab6018a55d76a02c1bf4f1fcd
Emily Kolar <emkolar@gmail.com>
2020-03-13 22:45:04 -0700

    add ramda util lib

--- src/util.js
+++ src/util.js
@@ -1,3 +1,7 @@
+import * as R from 'ramda';
+
 export const coinFlip = () => Math.floor(Math.random() * 2) === 0;
 
 export const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+export const isEq = (a, b) => R.equals(a, b);
