ebe39d7dcf85461c433328c3be9b6f42c1045bfa
Emily Kolar <emkolar@gmail.com>
2020-03-15 21:46:13 -0700

    draw weird 3-sided

--- src/BoundaryExperiment/BoundaryExperiment.js
+++ src/BoundaryExperiment/BoundaryExperiment.js
@@ -1,25 +1,55 @@
 import React from 'react';
 
 function draw(ctx, locations) {
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = 'deepskyblue';
-  ctx.shadowBlur = 20;
-  ctx.lineWidth = 4;
-  ctx.clearRect(0, 0, 600, 600);
-  locations.forEach((loc, idx) => {
-    ctx.moveTo(loc.x, loc.y);
-    const nextLoc = locations[idx + 1];
-    if (nextLoc) {
-      ctx.lineTo(nextLoc.x, nextLoc.y);
-      ctx.stroke();
-    }
-  });
+  if (locations[1]) {
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'deepskyblue';
+    ctx.shadowBlur = 20;
+    ctx.lineWidth = 4;
+    ctx.clearRect(0, 0, 600, 600);
+
+    const { x: x0, y: y0 } = locations[0];
+    const { x: x1, y: y1 } = locations[1];
+    const drawLocations = [{
+      x: x0,
+      y: y0,
+    }, {
+      x: x0,
+      y: y1,
+    }, {
+      x: x1,
+      y: y1,
+    }, {
+      x: x1,
+      y: y0,
+    }, {
+      x: x0,
+      y: y0,
+    }];
+    drawLocations.forEach((loc, idx) => {
+      const nextLoc = drawLocations[idx + 1];
+      if (nextLoc) {
+        ctx.lineTo(nextLoc.x, nextLoc.y);
+      }
+    });
+    ctx.stroke();
+  }
 }
 
 const BoundaryExperiment = () => {
   const canvasRef = React.useRef(null);
   const [locations, setLocations] = React.useState([]);
+
+  const [isDraw, setIsDraw] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isDraw) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      draw(ctx, locations);
+    }
+  }, [isDraw, locations]);
 
   return (
     <canvas
@@ -37,8 +67,10 @@ const BoundaryExperiment = () => {
         const loc = { x, y: y };
         const nextLocations = [...locations, loc];
         setLocations(nextLocations);
-        console.log(nextLocations);
-        draw(ctx, nextLocations);
+
+        if (!isDraw) {
+          setIsDraw(true);
+        }
       }}
     />
   );
