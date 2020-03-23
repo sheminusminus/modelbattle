7bdb205d155696c2140bf8ba81683709d6df8840
Emily Kolar <emkolar@gmail.com>
2020-03-22 23:52:12 -0700

    cleanup, typedefs

--- src/BoundaryExperiment/components/Svg.js
+++ src/BoundaryExperiment/components/Svg.js
@@ -7,6 +7,15 @@ import Point from './Point';
 import Rect from './Rect';
 import Text from './Text';
 
+/**
+ * @param {Object} options
+ * @param {Point[]} options.points
+ * @param {number} options.stablePtIndex
+ * @param {Point} options.stablePoint
+ * @param {number} options.dx
+ * @param {number} options.dy
+ * @return {Point[]}
+ */
 const mapPointsOnScale = ({
   points,
   stablePtIndex,
@@ -38,8 +47,98 @@ const mapPointsOnScale = ({
   };
 });
 
+/**
+ * @param {Object} options
+ * @param {number} options.dx
+ * @param {number} options.dy
+ * @param {Shape[]} options.prev
+ * @param {number} options.ptIndex
+ * @param {number} options.shapeIndex
+ * @return {Shape[]}
+ */
+const getNextShapesOnScale = (options) => {
+  const {
+    dx,
+    dy,
+    prev,
+    ptIndex,
+    shapeIndex,
+  } = options;
+
+  const nextDrawShapes = [...prev];
+
+  const points = prev[shapeIndex].points;
+  const halfPointsCount = points.length / 2;
+
+  let stablePtIndex = ptIndex - halfPointsCount;
+  if (stablePtIndex < 0) {
+    stablePtIndex = points.length + stablePtIndex;
+  }
+
+  const stablePoint = points[stablePtIndex];
+
+  nextDrawShapes[shapeIndex].points = mapPointsOnScale({
+    points,
+    stablePoint,
+    stablePtIndex,
+    dx,
+    dy,
+  });
+
+  return nextDrawShapes;
+};
+
+/**
+ * @param {number} shapeIndex
+ * @param {number} [ptIndex]
+ * @return {string}
+ */
+const getChildKey = (shapeIndex, ptIndex) => {
+  const key = `shape-${shapeIndex}`;
+  if (ptIndex) {
+    return `${key}-pt-${ptIndex}`;
+  }
+  return key;
+};
+
+const makeShapePointsStrAccum = (length) => (str, pt, idx) => {
+  return `${str}${pt.x},${pt.y}${idx < length - 1 ? ' ' : ''}`;
+};
+
+const getDataAttributesFromTarget = (target) => {
+  const dataActive = target.getAttribute('data-active');
+  const dataPoint = parseInt(target.getAttribute('data-point'), 10);
+  const dataShape = parseInt(target.getAttribute('data-shape'), 10);
+  return {
+    dataActive,
+    dataPoint,
+    dataShape,
+  };
+};
+
 const Svg = (props) => {
-  const { getRef, tags, shapes, width, height } = props;
+  const {
+    /**
+     * @type {Object}
+     */
+    getRef,
+    /**
+     * @type {number}
+     */
+    height,
+    /**
+     * @type {Shape[]}
+     */
+    shapes,
+    /**
+     * @type {Object}
+     */
+    tags,
+    /**
+     * @type {number}
+     */
+    width,
+  } = props;
 
   const [drawShapes, setDrawShapes] = React.useState(shapes);
   const [activePoint, setActivePoint] = React.useState({ shape: null, point: null });
@@ -69,29 +168,28 @@ const Svg = (props) => {
     const { dx, dy } = event;
 
     setDrawShapes((prev) => {
-      const nextDrawShapes = [...prev];
-
-      const points = prev[shapeIndex].points;
-      const halfPointsCount = points.length / 2;
-
-      let stablePtIndex = ptIndex - halfPointsCount;
-      if (stablePtIndex < 0) {
-        stablePtIndex = points.length + stablePtIndex;
-      }
-
-      const stablePoint = points[stablePtIndex];
-
-      nextDrawShapes[shapeIndex].points = mapPointsOnScale({
-        points,
-        stablePoint,
-        stablePtIndex,
+      return getNextShapesOnScale({
         dx,
         dy,
+        prev,
+        ptIndex,
+        shapeIndex,
       });
-
-      return nextDrawShapes;
     });
   }, []);
+
+  const pointMoveHandler = React.useCallback((event) => {
+    const {
+      dataActive,
+      dataPoint,
+      dataShape,
+    } = getDataAttributesFromTarget(event.target);
+    if (dataActive === 'true') {
+      onSinglePointMoved(event, dataShape, dataPoint);
+    } else {
+      onRectScaled(event, dataShape, dataPoint);
+    }
+  }, [onRectScaled, onSinglePointMoved]);
 
   const onPointHeld = React.useCallback((event, shapeIndex, ptIndex, isActive) => {
     if (isActive) {
@@ -100,6 +198,16 @@ const Svg = (props) => {
       setActivePoint({ shape: shapeIndex, point: ptIndex });
     }
   }, []);
+
+  const pointHeldHandler = React.useCallback((event) => {
+    const {
+      dataActive,
+      dataPoint,
+      dataShape,
+    } = getDataAttributesFromTarget(event.target);
+
+    onPointHeld(event, dataShape, dataPoint, dataActive === 'true');
+  }, [onPointHeld]);
 
   return (
     <svg
@@ -112,39 +220,19 @@ const Svg = (props) => {
       {drawShapes.map((shape, shapeIndex) => {
         const tag = tags[shape.tag] || {};
         const shapePoints = shape.points;
-        const points = shapePoints.reduce((str, pt, idx) => {
-          return `${str}${pt.x},${pt.y}${idx < shapePoints.length - 1 ? ' ' : ''}`;
-        }, '');
+        const reducer = makeShapePointsStrAccum(shapePoints.length);
+        const points = shapePoints.reduce(reducer, '');
 
         return (
-          <React.Fragment key={`shape-${shapeIndex}`}>
-            <Rect
-              points={points}
-              color={tag.color}
-            />
+          <React.Fragment key={getChildKey(shapeIndex)}>
+            <Rect points={points} color={tag.color} />
 
             {shapePoints.map((pt, ptIndex) => (
               <Point
                 color={tag.color}
-                key={`shape-${shapeIndex}-pt-${ptIndex}`}
-                onPointMoved={(event) => {
-                  const { target } = event;
-                  const dataActive = target.getAttribute('data-active');
-                  const dataPoint = parseInt(target.getAttribute('data-point'), 10);
-                  const dataShape = parseInt(target.getAttribute('data-shape'), 10);
-                  if (dataActive === 'true') {
-                    onSinglePointMoved(event, dataShape, dataPoint);
-                  } else {
-                    onRectScaled(event, dataShape, dataPoint);
-                  }
-                }}
-                onPointHeld={(event) => {
-                  const { target } = event;
-                  const dataActive = target.getAttribute('data-active');
-                  const dataPoint = parseInt(target.getAttribute('data-point'), 10);
-                  const dataShape = parseInt(target.getAttribute('data-shape'), 10);
-                  onPointHeld(event, dataShape, dataPoint, dataActive === 'true');
-                }}
+                key={getChildKey(shapeIndex, ptIndex)}
+                onPointMoved={pointMoveHandler}
+                onPointHeld={pointHeldHandler}
                 x={pt.x}
                 y={pt.y}
                 pointIndex={ptIndex}
---
+++ src/typedefs.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} Point
+ * @property {number} x
+ * @property {number} y
+ */
+
+/**
+ * @typedef {Object} Size
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @typedef {Object} Shape
+ * @property {Point[]} points
+ * @property {string} url
+ * @property {string} submitted
+ * @property {string} tag
+ * @property {Size} size
+ */
