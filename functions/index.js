614a2a4824ff8d29559e28c4d6cf16a6c665f919
Emily Kolar <emkolar@gmail.com>
2020-06-03 02:21:50 -0700

    (refactor) pass app to function creators; (add) body-parser

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,17 @@
-const exportBoundaryExperiments = require('./exportBoundaryExperiment');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const createExportAbExperiment = require('./exportAbExperiment');
+const createExportBoundaryExperiments = require('./exportBoundaryExperiment');
 
 
-module.exports.exportBoundaryExperiments = exportBoundaryExperiments;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+
+module.exports.exportAbExperiment = createExportAbExperiment(app);
+module.exports.exportBoundaryExperiments = createExportBoundaryExperiments(app);
